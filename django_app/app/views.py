acf0511b120b02ff2a6dd8044a72b3769e564c28
shivansh <shivanshsharma1305@gmail.com>
2022-01-23 13:41:17 +0530

    create a room and message model for app

--- django_app/app/views.py
+++ django_app/app/views.py
@@ -9,15 +9,26 @@ def home(request):
 def room(request) :
     return HttpResponese('welcome to room')   
 """
-rooms = [
+"""rooms = [
     {'id':1,'name':'i am shivansh sharma'},
     {'id':2,'name':'i am a data scientist'},
     {'id':3,'name':'i am a programmer'}
     ]
+"""
+
+from .models import Room
+
+
 def home(request):
+    rooms = Room.objects.all()
     return render(request,'app/home.html',{'rooms':rooms}) #we can also do this by calling app/template
     #return render(request,'home.html') ,we can also call home.html from root template folder 
 
 def room(request,pk):
-    
-    return render(request,'app/room.html')
+    rooms = Room.objects.get(id=int(pk))
+    """for i in rooms:
+        if i['id'] == int(pk):
+            Room = i['name']
+            break"""
+    context = {'room':rooms}
+    return render(request,'app/room.html',context)
